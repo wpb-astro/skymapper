3536b116b65b634b9f1cdfe77f5950eade504e21
Peter Melchior <peter.m.melchior@gmail.com>
2016-03-19 16:20:29 -0400

    introduces base class ConicProjection

--- skymapper.py
+++ skymapper.py
@@ -1,23 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-class AlbersEqualAreaProjection(object):
+class ConicProjection(object):
     def __init__(self, ra_0, dec_0, dec_1, dec_2):
-        """Albers Equal Area Projection.
+        """Base class for conic projections.
 
-        AEA is a conic projection with an origin along the lines connecting
-        the poles. It preserves relative area, but is not conformal,
-        perspective or equistant.
-
-        Its preferred use of for areas with predominant east-west extent
-        at moderate latitudes.
-
-        As a conic projection, it depends on two standard parallels, i.e.
-        intersections of the cone with the sphere. To minimize scale variations,
-        these standard parallels should be chosen as small as possible while
-        spanning the range in declinations of the data.
-
-        For details, see Snyder (1987, section 14).
+        A conic projection depends on two standard parallels, i.e.
+        intersections of the cone with the sphere.
+        For details, see Snyder (1987, page 97ff).
 
         Args:
             ra_0: RA that maps onto x = 0
@@ -30,47 +20,6 @@ class AlbersEqualAreaProjection(object):
         self.dec_1 = dec_1 # dec1 and dec2 only needed for __repr__
         self.dec_2 = dec_2
         self.deg2rad = np.pi/180
-        # Snyder 1987, eq. 14-3 to 14-6.
-        self.n = (np.sin(dec_1 * self.deg2rad) + np.sin(dec_2 * self.deg2rad)) / 2
-        self.C = np.cos(dec_1 * self.deg2rad)**2 + 2 * self.n * np.sin(dec_1 * self.deg2rad)
-        self.rho_0 = self.__rho__(dec_0)
-
-    def __rho__(self, dec):
-        return np.sqrt(self.C - 2 * self.n * np.sin(dec * self.deg2rad)) / self.n
-
-    def __call__(self, ra, dec, inverse=False):
-        """Convert RA/Dec into map coordinates, or the reverse.
-
-        Args:
-            ra:  float or array of floats
-            dec: float or array of floats
-            inverse: if True, convert from map coordinates to RA/Dec
-
-        Returns:
-            x,y with the same format as ra/dec
-        """
-        if not inverse:
-
-            ra_ = np.array([ra - self.ra_0]) * -1 # inverse for RA
-            # check that ra_ is between -180 and 180 deg
-            ra_[ra_ < -180 ] += 360
-            ra_[ra_ > 180 ] -= 360
-            # Snyder 1987, eq 14-1 to 14-4
-            theta = self.n * ra_[0]
-            rho = self.__rho__(dec)
-            return rho*np.sin(theta * self.deg2rad), self.rho_0 - rho*np.cos(theta * self.deg2rad)
-        else:
-            # ra/dec actually x/y
-            # Snyder 1987, eq 14-8 to 14-11
-            rho = np.sqrt(ra**2 + (self.rho_0 - dec)**2)
-            if self.n >= 0:
-                theta = np.arctan2(ra, self.rho_0 - dec) / self.deg2rad
-            else:
-                theta = np.arctan2(-ra, -(self.rho_0 - dec)) / self.deg2rad
-            return self.ra_0 - theta/self.n, np.arcsin((self.C - (rho * self.n)**2)/(2*self.n)) / self.deg2rad
-
-    def __repr__(self):
-        return "AlbersEqualAreaProjection(%r, %r, %r, %r)" % (self.ra_0, self.dec_0, self.dec_1, self.dec_2)
 
     def getMeridianPatches(self, meridians, **kwargs):
         """Get meridian lines in matplotlib format.
@@ -202,7 +151,76 @@ class AlbersEqualAreaProjection(object):
                 return None
         raise NotImplementedError("specify either RA or Dec")
 
-class LambertConformalProjection(object):
+
+class AlbersEqualAreaProjection(ConicProjection):
+    def __init__(self, ra_0, dec_0, dec_1, dec_2):
+        """Albers Equal Area Projection.
+
+        AEA is a conic projection with an origin along the lines connecting
+        the poles. It preserves relative area, but is not conformal,
+        perspective or equistant.
+
+        Its preferred use of for areas with predominant east-west extent
+        at moderate latitudes.
+
+        As a conic projection, it depends on two standard parallels, i.e.
+        intersections of the cone with the sphere. To minimize scale variations,
+        these standard parallels should be chosen as small as possible while
+        spanning the range in declinations of the data.
+
+        For details, see Snyder (1987, section 14).
+
+        Args:
+            ra_0: RA that maps onto x = 0
+            dec_0: Dec that maps onto y = 0
+            dec_1: lower standard parallel
+            dec_2: upper standard parallel (must not be -dec_1)
+        """
+        ConicProjection.__init__(self, ra_0, dec_0, dec_1, dec_2)
+
+        # Snyder 1987, eq. 14-3 to 14-6.
+        self.n = (np.sin(dec_1 * self.deg2rad) + np.sin(dec_2 * self.deg2rad)) / 2
+        self.C = np.cos(dec_1 * self.deg2rad)**2 + 2 * self.n * np.sin(dec_1 * self.deg2rad)
+        self.rho_0 = self.__rho__(dec_0)
+
+    def __rho__(self, dec):
+        return np.sqrt(self.C - 2 * self.n * np.sin(dec * self.deg2rad)) / self.n
+
+    def __call__(self, ra, dec, inverse=False):
+        """Convert RA/Dec into map coordinates, or the reverse.
+
+        Args:
+            ra:  float or array of floats
+            dec: float or array of floats
+            inverse: if True, convert from map coordinates to RA/Dec
+
+        Returns:
+            x,y with the same format as ra/dec
+        """
+        if not inverse:
+
+            ra_ = np.array([ra - self.ra_0]) * -1 # inverse for RA
+            # check that ra_ is between -180 and 180 deg
+            ra_[ra_ < -180 ] += 360
+            ra_[ra_ > 180 ] -= 360
+            # Snyder 1987, eq 14-1 to 14-4
+            theta = self.n * ra_[0]
+            rho = self.__rho__(dec)
+            return rho*np.sin(theta * self.deg2rad), self.rho_0 - rho*np.cos(theta * self.deg2rad)
+        else:
+            # ra/dec actually x/y
+            # Snyder 1987, eq 14-8 to 14-11
+            rho = np.sqrt(ra**2 + (self.rho_0 - dec)**2)
+            if self.n >= 0:
+                theta = np.arctan2(ra, self.rho_0 - dec) / self.deg2rad
+            else:
+                theta = np.arctan2(-ra, -(self.rho_0 - dec)) / self.deg2rad
+            return self.ra_0 - theta/self.n, np.arcsin((self.C - (rho * self.n)**2)/(2*self.n)) / self.deg2rad
+
+    def __repr__(self):
+        return "AlbersEqualAreaProjection(%r, %r, %r, %r)" % (self.ra_0, self.dec_0, self.dec_1, self.dec_2)
+
+class LambertConformalProjection(ConicProjection):
     def __init__(self, ra_0, dec_0, dec_1, dec_2):
         """Lambert Conformal Conic Projection.
 
@@ -226,12 +244,8 @@ class LambertConformalProjection(object):
             dec_1: lower standard parallel
             dec_2: upper standard parallel (must not be -dec_1)
         """
+        ConicProjection.__init__(self, ra_0, dec_0, dec_1, dec_2)
         # Snyder 1987, eq. 14-1, 14-2 and 15-1 to 15-3.
-        self.ra_0 = ra_0
-        self.dec_0 = dec_0
-        self.dec_1 = dec_1 # dec1 and dec2 only needed for __repr__
-        self.dec_2 = dec_2
-        self.deg2rad = np.pi/180
         self.dec_max = 89.99
 
         dec_1 *= self.deg2rad
@@ -280,138 +294,6 @@ class LambertConformalProjection(object):
 
     def __repr__(self):
         return "LambertConformalProjection(%r, %r, %r, %r)" % (self.ra_0, self.dec_0, self.dec_1, self.dec_2)
-
-    def getMeridianPatches(self, meridians, **kwargs):
-        """Get meridian lines in matplotlib format.
-
-        Meridian lines in conics are circular arcs, appropriate
-        matplotlib.patches will be return
-
-        Args:
-            meridians: list of declinations
-            **kwargs: matplotlib.patches.Arc parameters
-
-        Returns:
-            matplotlib.PatchCollection
-        """
-        from matplotlib.patches import Arc
-        from matplotlib.collections import PatchCollection
-
-        # get opening angle
-        origin = (0, self.rho_0)
-        top_left = self.__call__(self.ra_0 - 180, 90)
-        angle_limit = 180 - np.arctan2(origin[0]-top_left[0], origin[1]-top_left[1])/self.deg2rad
-        angle = 90
-        if self.n < 0:
-            angle = -90
-            angle_limit = 180 - angle_limit
-
-        # get radii
-        _, y = self.__call__(self.ra_0, meridians)
-        radius = np.abs(self.rho_0 - y)
-
-        patches = [Arc(origin, 2*radius[m], 2*radius[m], angle=angle, theta1=-angle_limit, theta2=angle_limit, **kwargs) for m in xrange(len(meridians))]
-        return PatchCollection(patches, match_original=True, zorder=patches[0].zorder)
-
-    def getParallelPatches(self, parallels, **kwargs):
-        """Get parallel lines in matplotlib format.
-
-        Parallel lines in conics are straight, appropriate
-        matplotlib.patches will be returned.
-
-        Args:
-            meridians: list of rectascensions
-            **kwargs: matplotlib.collection.LineCollection parameters
-
-        Returns:
-            matplotlib.LineCollection
-        """
-
-        # remove duplicates
-        parallels_ = np.unique(parallels % 360)
-
-        # the outer boundaries need to be duplicated because the same
-        # parallel appear on the left and the right side of the map
-        if self.ra_0 < 180:
-            outer = self.ra_0 - 180
-        else:
-            outer = self.ra_0 + 180
-        parallels_ = np.array(list(parallels_) + [outer])
-
-        from matplotlib.collections import LineCollection
-        top = self.__call__(parallels_, 90)
-        bottom = self.__call__(parallels_, -90)
-        x_ = np.dstack((top[0], bottom[0]))[0]
-        y_ = np.dstack((top[1], bottom[1]))[0]
-        return LineCollection(np.dstack((x_, y_)), color='k', **kwargs)
-
-    def findIntersectionAtX(self, x, ylim, ra=None, dec=None):
-        """Find intersection of meridian or parallel with a vertical line.
-
-        Args:
-            x: x coordinate of the vertical line
-            ylim: range in y for the vertical line
-            ra: if not None, search for a parallel at this ra to intersect
-            dec: if not None, search for a meridian at this dec to intersect
-
-        Returns:
-            float or None (if not solution was found)
-        """
-        if dec is not None:
-            # analytic solution for intersection of circle with line at x
-            r = np.abs(self.rho_0 - self.__call__(self.ra_0, dec)[1])
-            if np.abs(x) > np.abs(r):
-                return None
-            if self.rho_0 >= 0:
-                return self.rho_0 - np.sqrt(r**2 - x**2)
-            else:
-                return np.sqrt(r**2 - x**2) + self.rho_0
-        if ra is not None:
-            # analytic solution for intersection of line with line at y
-            top = self.__call__(ra, 90)
-            bottom = self.__call__(ra, -90)
-            delta = (top[0] - bottom[0], top[1] - bottom[1])
-            y = bottom[1] + (x - bottom[0]) * delta[1] / delta[0]
-            if y >= ylim[0] and y <= ylim[1]:
-                return y
-            else:
-                return None
-        raise NotImplementedError("specify either RA or Dec")
-
-    def findIntersectionAtY(self, y, xlim, ra=None, dec=None):
-        """Find intersection of meridian or parallel with a horizontal line.
-
-        Args:
-            y: y coordinate of the horizontal line
-            xlim: range in x for the horizontal line
-            ra: if not None, search for a parallel at this ra to intersect
-            dec: if not None, search for a meridian at this dec to intersect
-
-        Returns:
-            float or None (if not solution was found)
-        """
-        if dec is not None:
-            # analytic solution for intersection of circle with line at x
-            r = np.abs(self.rho_0 - self.__call__(self.ra_0, dec)[1])
-            if np.abs(y) > np.abs(r):
-                return None
-            if self.rho_0 >= 0:
-                return self.rho_0 - np.sqrt(r**2 - y**2)
-            else:
-                return np.sqrt(r**2 - y**2) + self.rho_0
-        if ra is not None:
-            # analytic solution for intersection of line with line at y
-            top = self.__call__(ra, 90)
-            bottom = self.__call__(ra, -90)
-            delta = (top[0] - bottom[0], top[1] - bottom[1])
-            x = bottom[0] + (y - bottom[1]) * delta[0] / delta[1]
-            if x >= xlim[0] and x <= xlim[1]:
-                return x
-            else:
-                return None
-        raise NotImplementedError("specify either RA or Dec")
-
-
 
 ##### Start of free methods #####
 
