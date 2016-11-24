69c188e1fa1b417656f1ffc64504cb2685bf06dc
Peter Melchior <peter.m.melchior@gmail.com>
2016-11-24 18:05:12 -0500

    extended setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
 setup(name="skymapper",
-            version='1.0',
-            author="Peter Melchior",
-            author_email="peter.m.melchior@gmail.com",
-            py_modules=["skymapper"],
-            )
+      description="Mapping astronomical survey data on the sky, handsomely",
+      long_description="Mapping astronomical survey data on the sky, handsomely",
+      version="0.1",
+      license="MIT",
+      author="Peter Melchior",
+      author_email="peter.m.melchior@gmail.com",
+      py_modules=["skymapper"],
+      url="https://github.com/pmelchior/skymapper",
+      requires=["matplotlib", "numpy"]
+)
 
