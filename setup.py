bf1b5c6ef14a14f1432ca7c25f46cf98da72c432
Peter Melchior <peter.m.melchior@gmail.com>
2020-03-25 23:44:59 -0400

    v0.4.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Mapping astronomical survey data on the sky, handsomely",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version="0.4.0",
+    version="0.4.1",
     license="MIT",
     author="Peter Melchior",
     author_email="peter.m.melchior@gmail.com",
